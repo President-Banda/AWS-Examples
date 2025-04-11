e7f695e5e80febec0b33ba15f52f37da1a76262a
president <abanda435@gmail.com>
2025-04-11 13:58:32 +0200

    feature: add s3 create bucket using ruby sdk

--- s3/sdk/ruby/s3.rb
+++ s3/sdk/ruby/s3.rb
@@ -1,1 +1,40 @@
-sample
+
+require 'aws-sdk-s3'
+require 'pry'
+require 'securerandom'
+
+bucket_name = ENV['BUCKET_NAME']
+region = 'ca-central-1'
+puts bucket_name
+
+client = Aws::S3::Client.new
+
+resp = client.create_bucket({
+    bucket: bucket_name,
+    create_bucket_configuration:{
+        location_constraint: region
+    }
+})
+
+# binding.pry # for debugging
+
+number_of_files = 1+rand(6)
+puts "number_of_files: #{number_of_files}"
+
+number_of_files.times.each do |i|
+    puts "i: #{i}"
+    filename = "file_#{i}.txt"
+    output_path="/tmp/#{filename}"
+
+    File.open(output_path, "w") do |file|
+        file.write SecureRandom.uuid
+    end
+
+    File.open(output_path, 'rb') do |file|
+        client.put_object(
+            bucket: bucket_name,
+            key: filename,
+            body: file
+            )
+    end
+end
